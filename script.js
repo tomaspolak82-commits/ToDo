5986b47765268384f66f5afa0857e7d9e832731e
Tomas Polak <tomaspolak82@gmail.com>
2026-06-07 07:56:00 +0200

    vytvoření functions, zákaz probublávání, přidání tlačítka mazat do form

--- script.js
+++ script.js
@@ -3,15 +3,29 @@
     const btnNewTask = document.querySelector("#newTask")
     const overlay = document.querySelector("#overlay")
     const NewTaskInForm = document.querySelector ("#newTaskInForm")
+    const modalWindows = document.querySelector("#modalNewTask") 
     btnNewTask.addEventListener("click", function (event) {
         overlay.classList.remove("invisible")
         overlay.classList.add("visible")
     } )
-    NewTaskInForm.addEventListener ("click", function(event) {
-        event.preventDefault()
-        console.log ("test")
+
+    modalWindows.addEventListener("click", function(event){
+        event.stopPropagation() // zabrání probublávání nahoru => nepřepne styl na overlay
     })
 
+    NewTaskInForm.addEventListener ("click", function(event) {
+        event.preventDefault()
+        console.log (event)
+    })
+
+
+  
+
+
+
+
+
+    // === zavře overlay => dát až na závěr po vyplnění formuláře
     overlay.addEventListener("click", function (event) {
         overlay.classList.remove("visible")
         overlay.classList.add("invisible")
